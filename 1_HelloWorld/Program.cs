4dc4e6caa52a2ec651c3a6f00e881c38b255fdd5
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-02-12 22:14:25 -0300

    Iniciando desenvolvimento .NOT na FIAP com o professor Leonardo Gasparini!

---
+++ 1_HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+var texto = Console.ReadLine();
+// $ em string interpolation, permite a interpolação de variáveis
+Console.WriteLine($"Texto digitado:{texto}");
